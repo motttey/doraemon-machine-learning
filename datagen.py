ec8ec44704e88d0f45592577d212d9bf2923f454
motttey <motitago@gmail.com>
2019-05-28 23:11:11 +0900

    modify datagen params

--- datagen.py
+++ datagen.py
@@ -6,7 +6,7 @@ import os
 import glob
 
 input_dir = "dd/doraimages2"
-output_dir = "dd/doraimages_new"
+output_dir = "dd/doraimages_new/imgs3"
 
 files = glob.glob(input_dir + '/*.jpg')
 
@@ -21,13 +21,15 @@ for i, file in enumerate(files):
     x = np.expand_dims(x, axis=0)
 
     datagen = ImageDataGenerator(
-        rotation_range=30,
-        width_shift_range=0.1,
-        zoom_range=0.1,
+        rotation_range=45,
+        horizontal_flip=True,
+        width_shift_range=0.05,
+        zoom_range=[0.8, 1.1],
         shear_range=0.1,
-        channel_shift_range=20,
+        channel_shift_range=50,
+        brightness_range=[0.7, 1.0],
     )
 
     g = datagen.flow(x, batch_size=1, save_to_dir=output_dir, save_prefix='', save_format='jpg')
-    for index in range(64):
+    for index in range(24):
         batch = g.next()
