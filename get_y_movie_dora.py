6af48cda674fa97e20337152392877aa39d6e2ec
motttey <motitago@gmail.com>
2020-10-21 21:39:35 +0900

    get title and rate

--- get_y_movie_dora.py
+++ get_y_movie_dora.py
@@ -3,17 +3,29 @@ from bs4 import BeautifulSoup
 
 url_base = "https://movies.yahoo.co.jp/movie"
 def get_rating(movie_url_list):
+    movie_rating_list = []
     for movie_url in movie_url_list:
+        movie_rating = {}
+
         r = requests.get(url_base + movie_url)
         bs = BeautifulSoup(r.text, "html.parser")
 
         mv_div = bs.find("div", {"id": "mv"})
         mv_data = mv_div.find("div", {"class": "movie_data"})
+        movie_title = mv_div.find("h1").find("span").text
+
         rader_label = mv_data.find("canvas", {"class": "rader-chart__figure"})
 
-        print(rader_label.get("data-chart-label"))
-        print(rader_label.get("data-chart-val-total"))
-    return
+        chart_label = rader_label.get("data-chart-label").split(",")
+        chart_val = rader_label.get("data-chart-val-total").split(",")
+
+        movie_rating["title"] = movie_title
+        movie_rating["rate"] = {}
+        for lavel, val in zip(chart_label, chart_val):
+            movie_rating["rate"][lavel] = float(val) if val != '' else 0
+
+        movie_rating_list.append(movie_rating)
+    return movie_rating_list
 
 def get_movies():
     movie_url_list = []
@@ -40,4 +52,5 @@ def get_movies():
 
 if __name__ == "__main__":
     movie_url_list = get_movies()
-    get_rating(movie_url_list)
+    movie_rating_list = get_rating(movie_url_list)
+    print(movie_rating_list)
