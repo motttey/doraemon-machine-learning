a117aee772ad113cf97b80773701e980cd1b021e
motttey <motitago@gmail.com>
2020-10-24 20:59:41 +0900

    plot_results using tsne

---
+++ get_dr_movie_dora.py
@@ -0,0 +1,62 @@
+# -*- Coding: utf-8 -*-
+import cv2
+import pathlib
+import numpy as np
+from PIL import Image
+from sklearn.decomposition import PCA
+from sklearn.manifold import TSNE
+import codecs
+import json
+
+import matplotlib.pyplot as plt
+from matplotlib import rcParams
+rcParams['font.family'] = 'sans-serif'
+rcParams['font.sans-serif'] = ['Hiragino Maru Gothic Pro', 'Noto Sans CJK JP']
+
+def remove_duplicate(movie_json):
+    titles = []
+    temp = []
+    for movie in movie_json:
+        if movie['title'] not in titles:
+            temp.append(movie)
+            titles.append(movie['title'])
+    return temp
+
+def show2DPlot(projected_array, path_list):
+    x = []
+    y = []
+    for point in projected_array:
+        x.append(point[0])
+        y.append(point[1])
+
+    fig,ax = plt.subplots(figsize=(12,8))
+    ax.set_xlabel('')
+    ax.set_ylabel('')
+
+    scatterplot = ax.scatter(x,y)
+
+    for i, txt in enumerate(path_list):
+        ax.annotate(txt, (x[i], y[i]))
+
+    plt.show()
+    return
+
+def main():
+    f = codecs.open('dora_movies.json', 'r', 'utf-8')
+    movie_json = remove_duplicate(json.load(f))
+
+    title_list = [movie['title'] for movie in movie_json]
+    target_data = []
+    for movie in movie_json:
+        values_array = [value for key, value in movie['rate'].items()]
+        target_data.append(values_array)
+
+    # t-SNE適用
+    tsne = TSNE(n_components=2)
+    embedded_data = tsne.fit_transform(target_data)
+    print(title_list)
+
+    show2DPlot(embedded_data, title_list)
+
+if __name__ == '__main__':
+    main()
